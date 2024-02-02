acd206b6117af7345f7493f093d415b7967f6b5e
gcardenasortiz <156359594+gcardenasortiz@users.noreply.github.com>
2024-02-01 16:10:57 -0800

    Ex

--- ArrayTests.java
+++ ArrayTests.java
@@ -7,6 +7,10 @@ public class ArrayTests {
     int[] input1 = { 3 };
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
+
+    int[] input2 = {3, 2, 1 };
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{1, 2, 3}, input2);
 	}
 
 
